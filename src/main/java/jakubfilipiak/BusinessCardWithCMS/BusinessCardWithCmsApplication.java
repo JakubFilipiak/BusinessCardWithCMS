92e3e13fee97eb7dc2a05db607f98d010b13ece2
JakubFilipiak <kuba.filipiak52@gmail.com>
2019-09-19 21:50:40 +0200

    Initial commit

---
+++ src/main/java/jakubfilipiak/BusinessCardWithCMS/BusinessCardWithCmsApplication.java
@@ -0,0 +1,13 @@
+package jakubfilipiak.BusinessCardWithCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCardWithCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCardWithCmsApplication.class, args);
+	}
+
+}
