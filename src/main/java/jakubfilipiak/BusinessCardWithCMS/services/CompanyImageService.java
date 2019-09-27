55b179cf1c6bafe66180738640121c72f09b02c6
JakubFilipiak <kuba.filipiak52@gmail.com>
2019-09-27 11:33:31 +0200

    Create empty services for entities

---
+++ src/main/java/jakubfilipiak/BusinessCardWithCMS/services/CompanyImageService.java
@@ -0,0 +1,11 @@
+package jakubfilipiak.BusinessCardWithCMS.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Jakub Filipiak on 27.09.2019
+ */
+@Service
+public class CompanyImageService {
+
+}
